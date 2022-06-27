67bcda13138a06f97400835691f7ab5bf51f9eb2
Joshkinz <jdeastin02@gmail.com>
2022-06-27 13:04:29 -0500

    attempts at using function

--- weapontracker.js
+++ weapontracker.js
@@ -1,6 +1,35 @@
 let totalTrue = 0;
 
-const HYD_WEPS = 19;
+const HYD_WEPS = [
+    {
+        id: 'hydCheck',
+        clss: 'hydPLD',
+        funct: updateHydaelyn,
+        src: 'images\icons\tank\PLD.png',
+        name: 'Bastard Sword of Divine Light',
+    }
+]
+
+
+function addWeaponInputRow(idName, className, functName, srcName, wepName) {
+    let checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', idName);
+    checkbox.setAttribute('class', className);
+    checkbox.setAttribute('onClick', functName);
+
+    let jobIcon = document.createElement('img');
+    jobIcon.setAttribute('id', 'classIcon');
+    jobIcon.setAttribute('src', srcName);
+
+    let name = document.createElement('label');
+    name.setAttribute('class', 'className');
+    name.innerHTML = wepName;
+
+    document.body.appendChild('checkbox');
+    document.body.appendChild('jobIcon');
+    document.body.appendChild('name');
+}
 
 function updateCount() {
     let count = document.querySelectorAll('input[type="checkbox"]:checked').length;
@@ -10,7 +39,7 @@ function updateCount() {
 
 function updateHydaelyn() {
     let count = document.querySelectorAll('input[id="hydCheck"]:checked').length;
-    document.getElementById("hydPercent").innerHTML = "Hydaelyn -  " + Math.trunc(count / HYD_WEPS * 100) + "%";
+    document.getElementById("hydPercent").innerHTML = "Hydaelyn -  " + Math.trunc(count / HYD_WEPS.length * 100) + "%";
 }
 
 function clickHydaelyn() {
