56d9b55a69740642a1a2e57304bcf2929e0e8cfc
Joshkinz <jdeastin02@gmail.com>
2022-06-28 13:05:45 -0500

    generate weapons from object

--- weapontracker.js
+++ weapontracker.js
@@ -4,12 +4,60 @@ const HYD_WEPS = [
     {
         id: 'hydCheck',
         clss: 'hydPLD',
-        funct: updateHydaelyn,
-        src: 'images\icons\tank\PLD.png',
+        funct: 'clickHydaelyn()',
+        src: 'images/icons/tank/PLD.png',
         name: 'Bastard Sword of Divine Light',
+    },
+    {
+        id: 'hydCheck',
+        clss: 'hydWAR',
+        funct: 'clickHydaelyn()',
+        src: 'images/icons/tank/WAR.png',
+        name: 'Labrys of Divine Light',
+    },
+    {
+        id: 'hydCheck',
+        class: 'hydDRK',
+        funct: 'clickHydaelyn()',
+        src: 'images/icons/tank/DRK.png',
+        name: 'Greatsword of Divine Light',
+    },
+    {
+        id: 'hydCheck',
+        class: 'hydGNB',
+        funct: 'clickHydaelyn()',
+        src: 'images/icons/tank/GNB.png',
+        name: 'Gunblade of Divine Light',
+    },
+    {
+        id: 'hydCheck',
+        class: 'hydWHM',
+        funct: 'clickHydaelyn()',
+        src: 'images/icons/healer/WHM.png',
+        name: 'Cane of Divine Light',
+    },
+    {
+        id: 'hydCheck',
+        class: 'hydSCH',
+        funct: 'clickHydaelyn()',
+        src: 'images/icons/healer/SCH.png',
+        name: 'Codex of Divine Light',
+    },
+    {
+        id: 'hydCheck',
+        class: 'hydAST',
+        funct: 'clickHydaelyn()',
+        src: 'images/icons/healer/AST.png',
+        name: 'Torquetum of Divine Light',
+    },
+    {
+        id: 'hydCheck',
+        class: 'hydSGE',
+        funct: 'clickHydaelyn()',
+        src: 'images/icons/healer/SGE.png',
+        name: 'Wings of Divine Light',
     }
 ]
-
 
 function addWeaponInputRow(idName, className, functName, srcName, wepName) {
     let checkbox = document.createElement('input');
@@ -26,15 +74,22 @@ function addWeaponInputRow(idName, className, functName, srcName, wepName) {
     name.setAttribute('class', 'className');
     name.innerHTML = wepName;
 
-    document.body.appendChild('checkbox');
-    document.body.appendChild('jobIcon');
-    document.body.appendChild('name');
+    const element = document.getElementById("hydaelyn")
+    element.appendChild(checkbox);
+    element.appendChild(jobIcon);
+    element.appendChild(name);
+}
+
+function drawHydWeps() {
+    HYD_WEPS.forEach(weapon => {
+        addWeaponInputRow(weapon.id, weapon.clss, weapon.funct, weapon.src, weapon.name);
+    })
+    console.log('Clicked!');
 }
 
 function updateCount() {
     let count = document.querySelectorAll('input[type="checkbox"]:checked').length;
     document.getElementById("obtained").innerHTML = "Total Obtained: " + count;
-
 }
 
 function updateHydaelyn() {
