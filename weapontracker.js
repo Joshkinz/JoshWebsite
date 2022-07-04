1fa41d0bca12a3ad43c04795e3bba7859f358232
Joshkinz <jdeastin02@gmail.com>
2022-07-03 23:37:56 -0500

    fixed cookies

--- weapontracker.js
+++ weapontracker.js
@@ -1,16 +1,14 @@
-let totalTrue = 0;
-
 const HYD_WEPS = [
     {
         id: 'hydCheck',
-        clss: 'hydPLD',
+        class: 'hydPLD',
         boss: 'HYDAELYN',
         src: 'images/icons/tank/PLD.png',
         name: 'Bastard Sword of Divine Light',
     },
     {
         id: 'hydCheck',
-        clss: 'hydWAR',
+        class: 'hydWAR',
         boss: 'HYDAELYN',
         src: 'images/icons/tank/WAR.png',
         name: 'Labrys of Divine Light',
@@ -139,17 +137,136 @@ const HYD_WEPS = [
 const END_WEPS = [
     {
         id: 'endCheck',
-        clss: 'endPLD',
+        class: 'endPLD',
         boss: 'ENDSINGER',
         src: 'images/icons/tank/PLD.png',
         name: 'Bluefeather Sword',
     },
     {
         id: 'endCheck',
-        clss: 'endWAR',
+        class: 'endWAR',
         boss: 'ENDSINGER',
         src: 'images/icons/tank/WAR.png',
         name: 'Bluefeather Axe',
+    },
+    {
+        id: 'endCheck',
+        class: 'endDRK',
+        boss: 'ENDSINGER',
+        src: 'images/icons/tank/DRK.png',
+        name: 'Bluefeather Faussar',
+    },
+    {
+        id: 'endCheck',
+        class: 'endGNB',
+        boss: 'ENDSINGER',
+        src: 'images/icons/tank/GNB.png',
+        name: 'Bluefeather Bayonet',
+    },
+    {
+        id: 'endCheck',
+        class: 'endWHM',
+        boss: 'ENDSINGER',
+        src: 'images/icons/healer/WHM.png',
+        name: 'Bluefeather Wand',
+    },
+    {
+        id: 'endCheck',
+        class: 'endSCH',
+        boss: 'ENDSINGER',
+        src: 'images/icons/healer/SCH.png',
+        name: 'Bluefeather Codex',
+    },
+    {
+        id: 'endCheck',
+        class: 'endAST',
+        boss: 'ENDSINGER',
+        src: 'images/icons/healer/AST.png',
+        name: 'Bluefeather Torquetum',
+    },
+    {
+        id: 'endCheck',
+        class: 'endSGE',
+        boss: 'ENDSINGER',
+        src: 'images/icons/healer/SGE.png',
+        name: 'Bluefeather Wings',
+    },
+    {
+        id: 'endCheck',
+        class: 'endMNK',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/MNK.png',
+        name: 'Bluefeather Tonfa',
+    },
+    {
+        id: 'endCheck',
+        class: 'endDRG',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/DRG.png',
+        name: 'Bluefeather Halberd',
+    },
+    {
+        id: 'endCheck',
+        class: 'endNIN',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/NIN.png',
+        name: 'Bluefeather Knives',
+    },
+    {
+        id: 'endCheck',
+        class: 'endSAM',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/SAM.png',
+        name: 'Bluefeather Tachi',
+    },
+    {
+        id: 'endCheck',
+        class: 'endRPR',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/RPR.png',
+        name: 'Bluefeather Zaghnal',
+    },
+    {
+        id: 'endCheck',
+        class: 'endBRD',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/BRD.png',
+        name: 'Bluefeather Longbow',
+    },
+    {
+        id: 'endCheck',
+        class: 'endMCH',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/MCH.png',
+        name: 'Bluefeather Musketoon',
+    },
+    {
+        id: 'endCheck',
+        class: 'endDNC',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/DNC.png',
+        name: 'Bluefeather Doomsbanes',
+    },
+    {
+        id: 'endCheck',
+        class: 'endBLM',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/BLM.png',
+        name: 'Bluefeather Rod',
+    },
+    {
+        id: 'endCheck',
+        class: 'endSMN',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/SMN.png',
+        name: 'Bluefeather Grimoire',
+    },
+    {
+        id: 'endCheck',
+        class: 'endRDM',
+        boss: 'ENDSINGER',
+        src: 'images/icons/dps/RDM.png',
+        name: 'Bluefeather Rapier',
     },
 ]
 
@@ -167,9 +284,20 @@ const ENDSINGER = {
     array: END_WEPS,
 }
 
-function updateCount(bossCheck, bossPercent, bossName, arrayName, obtainedName) {
+function updatePercent(arrayName) {
+    let percentCount = document.querySelectorAll('input[id=' + arrayName.id + ']:checked').length;
+    document.getElementById(arrayName.class).innerHTML = arrayName.name + " - " + Math.trunc(percentCount / arrayName.array.length * 100) + "%";
+}
+
+function updateCount(bossCheck, bossPercent, bossName, arrayName, weaponClass) {
     let percentCount = document.querySelectorAll('input[id=' + bossCheck + ']:checked').length;
     document.getElementById(bossPercent).innerHTML = bossName + " - " + Math.trunc(percentCount / arrayName.length * 100) + "%";
+    if (getCookie(weaponClass) === 'hidden') {
+    setCookie(weaponClass, 'show', 30);
+    }
+    else {
+        setCookie(weaponClass, 'hidden', 30);
+    }
 }
 
 function addWeaponInputRow(idName, className, bossName, srcName, wepName, bossID) {
@@ -177,14 +305,19 @@ function addWeaponInputRow(idName, className, bossName, srcName, wepName, bossID
     checkbox.setAttribute('type', 'checkbox');
     checkbox.setAttribute('id', idName);
     checkbox.setAttribute('class', className);
-    checkbox.setAttribute('onClick', 'updateCount(' + bossName + '.id, ' + bossName + '.class, ' + bossName + '.name, ' + bossName + '.array, ' + bossName + '.obtained)');
+    classString = 
+    checkbox.setAttribute('onClick', 'updateCount(' + bossName + '.id, ' + bossName + '.class, ' + bossName + '.name, ' + bossName + '.array, ' + '"' + String(className) + '"' + ')');
+    if (getCookie(className) == 'show') {
+        console.log("Success!");
+        checkbox.setAttribute('checked', 'true');
+    }
 
     let jobIcon = document.createElement('img');
     jobIcon.setAttribute('id', 'classIcon');
     jobIcon.setAttribute('src', srcName);
 
     let name = document.createElement('label');
-    name.setAttribute('class', 'className');
+    name.setAttribute('class', className);
     name.innerHTML = wepName;
 
     const element = document.getElementById(bossID)
@@ -193,13 +326,41 @@ function addWeaponInputRow(idName, className, bossName, srcName, wepName, bossID
     element.appendChild(name);
 }
 
-function drawWeps(boss_weps, bossID) { //Figure out how to make this work with the boss_weps and bossID tags after work
+function drawWeps(boss_weps, bossID) {
     boss_weps.forEach(weapon => {
-        addWeaponInputRow(weapon.id, weapon.clss, weapon.boss, weapon.src, weapon.name, bossID);
+        addWeaponInputRow(weapon.id, weapon.class, weapon.boss, weapon.src, weapon.name, bossID);
+        if (getCookie(weapon.class) == "") {
+            setCookie(weapon.class, 'hidden', 30);
+        }
     })
 }
 
 function pageLoadDrawWeps() {
     drawWeps(HYD_WEPS, 'hydaelyn');
-    drawWeps(END_WEPS, 'endsinger')
+    updatePercent(HYDAELYN);
+    drawWeps(END_WEPS, 'endsinger');
+    updatePercent(ENDSINGER);
 }
+
+function setCookie(cname, cvalue, exdays) {
+    const date = new Date();
+    date.setTime(date.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+  }
